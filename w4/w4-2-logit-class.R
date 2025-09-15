1c03ecd7358b12936586c4b46511a0d60afc250c
Davide Proserpio <dade.pro@gmail.com>
2025-09-15 07:01:31 -0700

    logit exercise

---
+++ w4/w4-2-logit-class.R
@@ -0,0 +1,20 @@
+library(ggplot2)
+library(ggthemes)
+library(data.table)
+library(scales)
+library(stargazer)
+library(stringi)
+#setwd as the directory where this file is saved
+setwd(paste0(dirname(rstudioapi::getActiveDocumentContext()$path), "/airbnb-case"))
+
+
+airbnb = fread("w4-airbnb-case.csv.gz")
+head(airbnb)
+
+airbnb[, gem:=as.integer(star_rating>=4.5 & reviews_count>20)]
+table(airbnb$gem)
+
+#predict probability of being a gem using logistic regression
+m_logit = glm(gem ~ price + guests_included + city + room_type, data = airbnb, family = binomial)
+stargazer(m_logit, type = "text", 
+          omit.stat = c("f", "ser", "aic", "bic"))
